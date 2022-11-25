f1b3744de7c008d749b136a20e97e2e71114839f
Tonkoshkur.Uliana <mikityukus@gmail.com>
2022-11-25 17:39:45 +0200

    Ok

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-//        Robot robot = new Robot();
-//        CoffeRobot coffeRobot = new CoffeRobot();
-//        RobotDancer robotDancer = new RobotDancer();
-//        RobotCoocker robotCoocker = new RobotCoocker();
-//
-//        robot.work();
-//        coffeRobot.work();
-//        robotDancer.work();
-//        robotCoocker.work();
-//
-//        System.out.println("*****************************************");
-//
-//        Robot[] arr = {robot, coffeRobot, robotCoocker, robotDancer};
-//        for (int i = 0; i < arr.length; i++) {
-//            Robot c = arr[i];
-//            c.work();
-//        }
+        Robot robot = new Robot();
+        CoffeRobot coffeRobot = new CoffeRobot();
+        RobotDancer robotDancer = new RobotDancer();
+        RobotCoocker robotCoocker = new RobotCoocker();
+
+        robot.work();
+        coffeRobot.work();
+        robotDancer.work();
+        robotCoocker.work();
+
+        System.out.println("*****************************************");
+
+        Robot[] arr = {robot, coffeRobot, robotCoocker, robotDancer};
+        for (int i = 0; i < arr.length; i++) {
+            Robot c = arr[i];
+            c.work();
+        }
 
 
         Animal a = new Animal("Леопард", 20, 7);
